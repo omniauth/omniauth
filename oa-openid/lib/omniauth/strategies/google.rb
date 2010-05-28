aacc6cadfdf392f1f052129bfd8e9ce819a5ae49
Dylan Egan <me@dylanegan.com>
2010-05-28 15:06:50 -0700

    Strategies for Google.

--- oa-openid/lib/omniauth/strategies/google.rb
+++ oa-openid/lib/omniauth/strategies/google.rb
@@ -1,6 +1,6 @@
 module OmniAuth
   module Strategies
-    class Google < OmniAuth::Stratgies::OpenID
+    class Google < OmniAuth::Strategies::OpenID
       def identifier; 'https://www.google.com/accounts/o8/id' end
     end
   end
