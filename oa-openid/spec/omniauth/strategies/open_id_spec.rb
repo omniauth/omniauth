353f4085d4330f6f40f5a5e69a8930fd0fcd8cee
James A. Rosen <james.a.rosen@gmail.com>
2010-06-12 22:41:05 -0400

    added spec_helper for oa-openid, added stup spec for openid strategy

---
+++ oa-openid/spec/omniauth/strategies/open_id_spec.rb
@@ -0,0 +1,7 @@
+require File.expand_path(File.dirname(__FILE__) + '/../../spec_helper')
+
+describe OmniAuth::Strategies::OpenID do
+  it 'should exist' do
+    # do nothing
+  end
+end
---
+++ oa-openid/spec/spec_helper.rb
@@ -0,0 +1,10 @@
+require 'rubygems'
+require 'spec'
+require 'spec/autorun'
+require 'rack/test'
+require 'webmock/rspec'
+
+include Rack::Test::Methods
+include WebMock
+
+require 'omniauth/openid'
