7691e0c2953438762eec47fabd634cb28ce0e755
James A. Rosen <james.a.rosen@gmail.com>
2010-06-18 20:50:44 -0400

    updated OAuth spec helper: added Bundler.setup to support running specs individually; added configure block to do common mixins

--- oa-oauth/spec/spec_helper.rb
+++ oa-oauth/spec/spec_helper.rb
@@ -1,10 +1,14 @@
 require 'rubygems'
+require 'bundler'
+Bundler.setup
 require 'spec'
 require 'spec/autorun'
-require 'rack/test'
 require 'webmock/rspec'
-
-include Rack::Test::Methods
-include WebMock
-
+require 'rack/test'
+require 'omniauth/core'
 require 'omniauth/oauth'
+
+Spec::Runner.configure do |config|
+  config.include WebMock
+  config.include Rack::Test::Methods
+end
