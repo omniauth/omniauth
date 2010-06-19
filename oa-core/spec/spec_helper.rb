88a008314181447f6343c27634efe66e957d7aaf
James A. Rosen <james.a.rosen@gmail.com>
2010-06-18 23:19:00 -0400

    added Bundler to oa-core spec_helper to support single-spec runs

--- oa-core/spec/spec_helper.rb
+++ oa-core/spec/spec_helper.rb
@@ -1,10 +1,13 @@
 require 'rubygems'
+require 'bundler'
+Bundler.setup
 require 'spec'
 require 'spec/autorun'
 require 'rack/test'
-require 'webmock/rspec'
-
-include Rack::Test::Methods
-include WebMock
-
 require 'omniauth/core'
+require 'omniauth/test'
+
+Spec::Runner.configure do |config|
+  config.include Rack::Test::Methods
+  config.extend  OmniAuth::Test::StrategyMacros, :type => :strategy
+end
