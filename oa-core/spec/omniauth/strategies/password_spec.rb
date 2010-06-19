00de993ae4a12f286da8bdf24b041c2e8dfc4e3b
James A. Rosen <james.a.rosen@gmail.com>
2010-06-18 23:20:34 -0400

    added Password specs

--- oa-core/spec/omniauth/strategies/password_spec.rb
+++ oa-core/spec/omniauth/strategies/password_spec.rb
@@ -1,7 +1,39 @@
 require File.dirname(__FILE__) + '/../../spec_helper'
 
-describe OmniAuth::Strategies::Password do
-  it 'should exist' do
-    # do nothing
+describe OmniAuth::Strategies::Password, :type => :strategy do
+
+  include OmniAuth::Test::StrategyTestCase
+
+  def strategy
+    [OmniAuth::Strategies::Password, 'anything here', {:identifier_key => 'username'}]
   end
+  
+  describe 'GET /auth/password without a password' do
+    before do
+      get '/auth/password', { :identifier => 'jerome' }
+    end
+    it 'should be unauthorized' do
+      last_response.should be_redirect
+      redirect_to = OmniAuth.config.path_prefix + '/failure?message=missing_information'
+      last_response.headers['Location'].should == redirect_to
+    end
+  end
+  
+  describe 'GET /auth/password with a username and password' do
+    before do
+      get '/auth/password', { :identifier => 'jerome', :password => 'my password'}
+    end
+    it 'should be OK' do
+      last_response.should be_ok
+    end
+    sets_an_auth_hash
+    sets_provider_to 'password'
+    it 'should set the UID to an opaque identifier' do
+      uid = last_request['auth']['uid']
+      uid.should_not be_nil
+      uid.should_not =~ /jerome/
+      uid.should_not =~ /my password/
+    end
+  end
+
 end
