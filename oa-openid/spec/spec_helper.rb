16ed90dcc993bf3c552ac71464e6ce56a8273c73
James A. Rosen <james.a.rosen@gmail.com>
2010-07-03 18:07:05 -0400

    started OpenID spec -- still many failures, but not sure of best testing approach

--- oa-openid/spec/spec_helper.rb
+++ oa-openid/spec/spec_helper.rb
@@ -1,10 +1,16 @@
 require 'rubygems'
+require 'bundler'
+Bundler.setup
 require 'spec'
 require 'spec/autorun'
-require 'rack/test'
 require 'webmock/rspec'
-
-include Rack::Test::Methods
-include WebMock
-
+require 'rack/test'
+require 'omniauth/core'
+require 'omniauth/test'
 require 'omniauth/openid'
+
+Spec::Runner.configure do |config|
+  config.include WebMock
+  config.include Rack::Test::Methods
+  config.extend  OmniAuth::Test::StrategyMacros, :type => :strategy
+end
