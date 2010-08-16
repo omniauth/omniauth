f514c6cacc6623ce26f0094e4bf054d33a280e36
Michael Bleigh <michael@intridea.com>
2010-08-16 10:14:04 -0500

    Updating dependencies for latest WebMock.

--- development_dependencies.rb
+++ development_dependencies.rb
@@ -8,7 +8,7 @@ if Object.const_defined?(:Bundler) && Bundler.const_defined?(:Dsl) && self.kind_
     gem  'rake'
     gem  'mg',        '~> 0.0.8'
     gem  'rspec',     '~> 1.3.0'
-    gem  'webmock',   '~> 1.3.3'
+    gem  'webmock',   '~> 1.3.4'
     gem  'rack-test', '~> 0.5.4'
     gem  'json',      '~> 1.4.3' # multi_json implementation
   end  
@@ -16,7 +16,7 @@ else #gemspec
   gem.add_development_dependency  'rake'
   gem.add_development_dependency  'mg',         '~> 0.0.8'
   gem.add_development_dependency  'rspec',      '~> 1.3.0'
-  gem.add_development_dependency  'webmock',    '~> 1.3.3'
+  gem.add_development_dependency  'webmock',    '~> 1.3.4'
   gem.add_development_dependency  'rack-test',  '~> 0.5.4'
   gem.add_development_dependency  'json',       '~> 1.4.3' # multi_json implementation
 end
