3443188c24bf75065aff1a3de8cf8ec8b428c573
Ping Yu <ping@intridea.com>
2010-10-04 11:06:19 -0500

    added ldap to omni user_info mapping

--- oa-enterprise/lib/omniauth/strategies/ldap.rb
+++ oa-enterprise/lib/omniauth/strategies/ldap.rb
@@ -7,7 +7,17 @@ module OmniAuth
       include OmniAuth::Strategy
       
       autoload :Adaptor, 'omniauth/strategies/ldap/adaptor'
-      
+      puts Dir.entries(".")
+      @@config   =  {'first_name' => 'givenName', 'last_name' => 'sn', 'email' => ['mail', "email", 'userPrincipalName'],
+										'phone' => ['telephoneNumber', 'homePhone', 'facsimileTelephoneNumber'],
+										'mobile_number' => ['mobile', 'mobileTelephoneNumber'],
+										'nickname' => ['uid', 'userid', 'sAMAccountName'],
+										'title' => 'title',
+										'location' => {"%0, %1, %2, %3 %4" => [['address', 'postalAddress', 'homePostalAddress', 'street', 'streetAddress'], ['l'], ['st'],['co'],['postOfficeBox']]},
+										'uid' => 'dn',
+										'url' => ['wwwhomepage'],
+										'image' => 'jpegPhoto',
+										'description' => 'description'}
       def initialize(app, title, options = {})
         super(app, options.delete(:name) || :ldap)
         @title = title
@@ -33,7 +43,8 @@ module OmniAuth
       def perform
       	begin
       		@adaptor.bind(:bind_dn => request.POST['username'], :password => request.POST['password'])
-      		@user_info = @adaptor.search(:filter => Net::LDAP::Filter.eq(@adaptor.uid, request.POST['username']),:limit => 1)
+      		@ldap_user_info = @adaptor.search(:filter => Net::LDAP::Filter.eq(@adaptor.uid, request.POST['username']),:limit => 1)
+      		@user_info = self.class.map_user(@@config, @ldap_user_info)
       		puts @user_info
 	        request.POST['auth'] = auth_hash
 	        @env['REQUEST_METHOD'] = 'GET'
@@ -52,11 +63,35 @@ module OmniAuth
       
       def auth_hash
         OmniAuth::Utils.deep_merge(super, {
-          'uid' => @user_info["dn"],
-          'user_info' => @user_info
+          'uid' => @user_info["uid"],
+          'user_info' => @user_info,
+          'extra' => @ldap_user_info
         })
       end
       
+		  def self.map_user mapper, object
+		  	user = {}
+		    mapper.each do |key, value|
+		      case value
+		        when String
+		          user[key] = object[value.downcase.to_sym].to_s if object[value.downcase.to_sym]
+		        when Array
+		          value.each {|v| (user[key] = object[v.downcase.to_sym].to_s; break;) if object[v.downcase.to_sym]}
+		        when Hash
+		        	value.map do |key1, value1|
+			        	pattern = key1.dup
+			        	value1.each_with_index do |v,i|
+			          	part = '';
+			          	v.each {|v1| puts v1.inspect; (part = object[v1.downcase.to_sym].to_s; break;) if object[v1.downcase.to_sym]}
+			          	puts part
+			        		pattern.gsub!("%#{i}",part||'') 
+			        	end	
+			        	user[key] = pattern
+		       		end
+		        end
+		      end
+		    user
+		  end       
     end
   end
 end
