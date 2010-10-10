fef10e56a19ad51bf57078a7eac21453f96d796a
Les Hill <leshill@gmail.com>
2010-10-10 14:47:36 -0400

    Add spec for Twitter authentication url
    - this was broken until commit 5e8eb2839f40f5436850

--- oa-oauth/spec/omniauth/strategies/twitter_spec.rb
+++ oa-oauth/spec/omniauth/strategies/twitter_spec.rb
@@ -1,13 +1,20 @@
 require File.expand_path(File.dirname(__FILE__) + '/../../spec_helper')
 
 describe 'OmniAuth::Strategies::Twitter' do
-  
+
   it 'should subclass OAuth' do
     OmniAuth::Strategies::Twitter.should < OmniAuth::Strategies::OAuth
   end
-  
+
   it 'should initialize with just consumer key and secret' do
     lambda{OmniAuth::Strategies::Twitter.new({},'abc','def')}.should_not raise_error
   end
-  
+
+  let(:twitter) { OmniAuth::Strategies::Twitter.new({},'abc','def') }
+
+  # See http://dev.twitter.com/pages/sign_in_with_twitter
+  it 'uses the sign in endpoint' do
+    twitter.consumer.authorize_path.should == '/oauth/authenticate'
+    twitter.consumer.authorize_url.should == 'https://api.twitter.com/oauth/authenticate'
+  end
 end
