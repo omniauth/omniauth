15134c27d5da0c03128d5ce655f4dbad0e0f8796
eric <eric.redmond@gmail.com>
2010-10-20 12:24:02 -0700

    authorize_path prepended by site

--- oa-oauth/lib/omniauth/strategies/meetup.rb
+++ oa-oauth/lib/omniauth/strategies/meetup.rb
@@ -13,10 +13,10 @@ module OmniAuth
     class Meetup < OmniAuth::Strategies::OAuth
       def initialize(app, consumer_key, consumer_secret)
         super(app, :meetup, consumer_key, consumer_secret,
-                :site => 'https://api.meetup.com',
-                :request_token_path => "/oauth/request",
-                :access_token_path  => "/oauth/access",
-                :authorize_path    => "http://www.meetup.com/authorize/")
+                # :site => 'https://api.meetup.com',
+                :request_token_path => "https://api.meetup.com/oauth/request",
+                :access_token_path  => "https://api.meetup.com/oauth/access",
+                :authorize_path     => "http://www.meetup.com/authorize/")
       end
     end
   end
