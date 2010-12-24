36478ca0cd68c5ae2362f37740b55a7abe7862cf
Qi He <qihe229@gmail.com>
2010-12-24 15:05:53 -0800

    debug

--- oa-oauth/lib/omniauth/strategies/oauth.rb
+++ oa-oauth/lib/omniauth/strategies/oauth.rb
@@ -35,7 +35,6 @@ module OmniAuth
       end
     
       def callback_phase
-        raise session[:oauth].inspect
         request_token = ::OAuth::RequestToken.new(consumer, session[:oauth][name.to_sym].delete(:request_token), session[:oauth][name.to_sym].delete(:request_secret))
         
         opts = {}
