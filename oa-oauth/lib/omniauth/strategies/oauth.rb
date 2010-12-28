25ba1e3c2c0713b2d89b4b06b3abdb1548fc3b91
Qi He <qihe229@gmail.com>
2010-12-28 11:28:36 -0800

    debug

--- oa-oauth/lib/omniauth/strategies/oauth.rb
+++ oa-oauth/lib/omniauth/strategies/oauth.rb
@@ -42,6 +42,7 @@ module OmniAuth
         @access_token = request_token.get_access_token(opts)
         super
       rescue ::OAuth::Unauthorized => e
+        raise e.inspect
         fail!(:invalid_credentials, e)
       end
       
