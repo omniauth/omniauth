d6d1d6719ccde5261f9664a2bb3a8117851d7d35
Alex Kremer <alex@flextrip.com>
2011-02-10 06:13:07 +0800

    TripIt adjusted for Omniauth 0.2

--- oa-oauth/lib/omniauth/strategies/trip_it.rb
+++ oa-oauth/lib/omniauth/strategies/trip_it.rb
@@ -17,19 +17,6 @@ module OmniAuth
                 :access_token_path  => "/oauth/access_token",
                 :authorize_url     => "https://www.tripit.com/oauth/authorize"}, options, &block)
       end
-      
-      def request_phase
-        request_token = consumer.get_request_token(:oauth_callback => callback_url)
-        (session[:oauth]||={})[name.to_sym] = {:callback_confirmed => request_token.callback_confirmed?, :request_token => request_token.token, :request_secret => request_token.secret}
-        r = Rack::Response.new
-        # For some reason, TripIt NEEDS the &oauth_callback query param or the user receives an error.
-        r.redirect request_token.authorize_url + "&oauth_callback=" + urlencode(callback_url)
-        r.finish
-      end
-      
-      def urlencode(str)
-        str.gsub(/[^a-zA-Z0-9_\.\-]/n) { sprintf('%%%02x', $&[0].ord) }
-      end
     end
   end
 end
