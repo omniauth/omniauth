bcacff4353da603429e121517ba75ca6ed9c70df
Qi He <qihe229@gmail.com>
2011-02-11 21:31:02 -0800

    customize renren image

--- oa-cookie/lib/omniauth/strategies/renren/helper.rb
+++ oa-cookie/lib/omniauth/strategies/renren/helper.rb
@@ -20,8 +20,27 @@ module OmniAuth
             HTML
           end
         end
+        
+        def omniauth_simple_renren_connect_button(params = {})
+          image_path = params[:image_path] ? params[:image_path] : "http://pics.wanlibo.com/images_cn/registration/renren.png"
+          simple_renren_connect_button(image_path).html_safe
+        end
 
         private
+        
+        def simple_renren_connect_button(image_path)
+          callback_path = "#{OmniAuth.config.path_prefix}/renren/callback"
+          <<-HTML
+<img src="#{image_path}" title="renren connect" alt="renren connect" onclick="XN.Connect.requireSession(function(){window.location.href='#{callback_path}';});return false;"></img>        
+<script src="http://static.connect.renren.com/js/v1.0/FeatureLoader.jsp" type="text/javascript"></script>
+<script type="text/javascript">
+  //<![CDATA[
+  XN_RequireFeatures(['EXNML'], function(){ XN.Main.init('#{Renren.api_key}', '/xd_receiver.html'); });
+  //]]>
+</script>
+          HTML
+        end
+        
         def renren_connect_button
           <<-HTML
 <xn:login-button autologoutlink="true" onlogin="document.getElementById('#{@renren_connect_form_id}').submit();"></xn:login-button>
