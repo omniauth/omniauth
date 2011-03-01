bd0a5a98a1a14ec47892b7f33eed5811d179a60f
Michael Bleigh <michael@intridea.com>
2011-03-01 10:27:21 -0600

    Fixes Miso.

--- oa-oauth/lib/omniauth/strategies/miso.rb
+++ oa-oauth/lib/omniauth/strategies/miso.rb
@@ -12,23 +12,19 @@ module OmniAuth
     #    use OmniAuth::Strategies::Miso, 'consumerkey', 'consumersecret'
     #
     class Miso < OmniAuth::Strategies::OAuth
-      def initialize(app, consumer_key, consumer_secret)
-        super(app, :miso, consumer_key, consumer_secret,
-                :site => 'https://gomiso.com',
-                :authorize_path => '/oauth/authorize')
+      def initialize(app, consumer_key = nil, consumer_secret = nil, options = {}, &block)
+        super(app, :miso, consumer_key, consumer_secret, {:site => 'https://gomiso.com'}, options)
       end
       
       def auth_hash
         OmniAuth::Utils.deep_merge(super, {
-          'uid' => @access_token.params[:user_id],
+          'uid' => user_hash['id'],
           'user_info' => user_info,
           'extra' => {'user_hash' => user_hash}
         })
       end
       
-      def user_info
-        user_hash = self.user_hash["user"]
-        
+      def user_info 
         {
           'nickname' => user_hash['username'],
           'name' => user_hash['full_name'],
@@ -38,7 +34,7 @@ module OmniAuth
       end
       
       def user_hash
-        @user_hash ||= MultiJson.decode(@access_token.get('/api/oauth/v1/users/show.json').body)
+        @user_hash ||= MultiJson.decode(@access_token.get('/api/oauth/v1/users/show.json').body)['user']
       end
     end
   end
