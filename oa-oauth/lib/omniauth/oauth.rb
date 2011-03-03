f1bd9a490cbccd64140d2d672f685e46e1e4d18b
Szymon Nowak <szimek@gmail.com>
2011-03-03 13:43:33 +0100

    Added Evernote strategy.

--- oa-oauth/lib/omniauth/oauth.rb
+++ oa-oauth/lib/omniauth/oauth.rb
@@ -28,5 +28,6 @@ module OmniAuth
     autoload :YouTube,            'omniauth/strategies/you_tube'
     autoload :Hyves,              'omniauth/strategies/hyves'
     autoload :Miso,               'omniauth/strategies/miso'
+    autoload :Evernote,           'omniauth/strategies/evernote'
   end
 end
