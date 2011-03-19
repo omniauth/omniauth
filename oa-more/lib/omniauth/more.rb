05d7797d00d943a5fe7b1f6b8888629c8195fd98
chouti <chouti@gmail.com>
2011-03-19 15:28:29 +0800

    add yupoo as a provider

--- oa-more/lib/omniauth/more.rb
+++ oa-more/lib/omniauth/more.rb
@@ -4,5 +4,6 @@ module OmniAuth
   module Strategies
     autoload :WindowsLive, 'omniauth/strategies/windows_live'
     autoload :Flickr, 'omniauth/strategies/flickr'
+    autoload :Yupoo, 'omniauth/strategies/yupoo'
   end
 end
