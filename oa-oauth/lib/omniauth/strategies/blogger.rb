0c28f4db70a655f38a971f5fafa7219f7ce7949f
Diego Sueiro <dsueiro@backing-online.com>
2011-03-30 17:24:24 -0300

    Blogger integration up and running

--- oa-oauth/lib/omniauth/strategies/blogger.rb
+++ oa-oauth/lib/omniauth/strategies/blogger.rb
@@ -34,29 +34,19 @@ module OmniAuth
       end
 
       def user_info
-        debugger
-        entry = user_hash['entry']
         {
-          'uid' => entry['id']['$t'],
-          'nickname' => entry['author'].first['name']['$t'],
-          'first_name' => entry['yt$firstName'] && entry['yt$firstName']['$t'],
-          'last_name' => entry['yt$lastName'] && entry['yt$lastName']['$t'],
-          'image' => entry['media$thumbnail'] && entry['media$thumbnail']['url'],
-          'description' => entry['yt$description'] && entry['yt$description']['$t'],
-          'location' => entry['yt$location'] && entry['yt$location']['$t']
+          'uid' => user_hash['feed']['author'][0]['email']['$t'],
+          'nickname' => user_hash['feed']['author'][0]['name']['$t']
         }
       end
 
       def user_hash
-        # YouTube treats 'default' as the currently logged-in user
-        # via http://apiblog.youtube.com/2010/11/update-to-clientlogin-url.html
-        @user_hash ||= MultiJson.decode(@access_token.get("http://www.blogger.com/feeds/api/users/default?alt=json").body)
+        # Using Contact feed
+        @user_hash ||= MultiJson.decode(@access_token.get("https://www.google.com/m8/feeds/contacts/default/full/?alt=json").body)
       end
 
-      # Monkeypatch consumer.get_request_token but specify YouTube scope rather than Google Contacts
-      # TODO this is an easy patch to the underlying OAuth strategy a la OAuth2
       def request_phase
-        request_token = consumer.get_request_token({:oauth_callback => callback_url}, {:scope => 'http://www.blogger.com/feeds'})
+        request_token = consumer.get_request_token({:oauth_callback => callback_url}, {:scope => 'http://www.blogger.com/feeds/ http://www.google.com/m8/feeds/'})
         session['oauth'] ||= {}
         session['oauth'][name.to_s] = {'callback_confirmed' => request_token.callback_confirmed?, 'request_token' => request_token.token, 'request_secret' => request_token.secret}
         r = Rack::Response.new
