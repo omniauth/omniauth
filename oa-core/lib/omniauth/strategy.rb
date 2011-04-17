123791494155e530be8a181d1a3eb66c5866b0d7
Emil Tin <emil@tin.dk>
2011-04-17 22:56:14 +0200

    debugging a problem with invalid_credentials on facebook

--- oa-core/lib/omniauth/strategy.rb
+++ oa-core/lib/omniauth/strategy.rb
@@ -37,7 +37,7 @@ module OmniAuth
       
       if match_request_path && OmniAuth.config.allowed_request_methods.include?(request.request_method.downcase.to_sym)
 
-        puts '------------'
+        puts '------------ request --------------'
         puts "current_path: #{current_path}"
         puts "callback_path: #{callback_path}"
         puts "request.path_info: #{request.path_info}"
@@ -54,6 +54,11 @@ module OmniAuth
           request_phase
         end
       elsif match_callback_path
+        puts '------------ callback --------------'
+        puts "current_path: #{current_path}"
+        puts "callback_path: #{callback_path}"
+        puts "request.path_info: #{request.path_info}"
+
         setup_phase                  
         @env['omniauth.origin'] = session.delete('omniauth.origin')
         @env['omniauth.origin'] = nil if env['omniauth.origin'] == ''
@@ -127,7 +132,8 @@ module OmniAuth
       if options[:path_prefix]
         options[:callback_path] || "#{path_prefix}/#{name}/callback"
       else
-        options[:callback_path] || "#{current_path}/callback"
+        puts "constructing callback_path from: #{current_path}"
+        options[:callback_path] || "#{current_path.sub(/\/callback$/,'')}/callback"
       end
     end
 
