b124e599dce45611cc2266becb3152db0f5b42a1
Michael Bleigh <michael@intridea.com>
2011-04-25 19:27:33 -0500

    Actually support :mobile and :sign_in options for Foursquare strategy.

--- oa-oauth/lib/omniauth/strategies/foursquare.rb
+++ oa-oauth/lib/omniauth/strategies/foursquare.rb
@@ -9,11 +9,16 @@ module OmniAuth
       # @option options [Boolean, true] :sign_in When true, use a sign-in flow instead of the authorization flow.
       # @option options [Boolean, false] :mobile When true, use the mobile sign-in interface.
       def initialize(app, client_id = nil, client_secret = nil, options = {}, &block)
+        options[:sign_in] ||= true
         super(app, :foursquare, client_id, client_secret, {
           :site => "https://api.foursquare.com/v2",
-          :authorize_url      => "https://foursquare.com/oauth2/authenticate",
+          :authorize_url      => authorize_url(options),
           :access_token_url   => "https://foursquare.com/oauth2/access_token"
         }, options, &block)
+      end
+
+      def authorize_url(options)
+        "https://foursquare.com/#{'mobile/' if options[:mobile]}oauth2/#{options[:sign_in] ? 'authenticate' : 'authorize'}"
       end
 
       def user_data
