5d15e99649b35c18a27e110b161ef72938f8ef84
lexer <alexey.v.zaharov@gmail.com>
2011-04-29 12:00:18 +0600

    add empty user data

--- oa-oauth/lib/omniauth/strategies/mailru.rb
+++ oa-oauth/lib/omniauth/strategies/mailru.rb
@@ -35,6 +35,7 @@ module OmniAuth
       end      
 
       def user_data
+        @data = {}
         #@data ||= MultiJson.decode(@access_token.get("http://www.appsmail.ru/platform/api?method=users.getInfo&app_id=#{api_key}&session_key=#{@access_token['access_token']}&sig=#{secret_key}"))[0]
         #@data ||= MultiJson.decode(@access_token.get("http://www.appsmail.ru/platform/api?method=users.getInfo"))[0]        
       end
