55aebec0a7e4369a8ebd7d7effcc72d7f8ef0f48
AlexanderPavlenko <alexandr.pavlenko@flatsoft.com>
2011-07-25 13:20:17 +0400

    pass usefull options

--- oa-oauth/lib/omniauth/strategies/oauth2/vkontakte.rb
+++ oa-oauth/lib/omniauth/strategies/oauth2/vkontakte.rb
@@ -66,7 +66,9 @@ module OmniAuth
         token = super
         # indicates that `offline` permission was granted, no need to the token refresh
         if token.expires_in == 0
-          ::OAuth2::AccessToken.new(token.client, token.token)
+          ::OAuth2::AccessToken.new(token.client, token.token,
+            token.params.reject{|k,_| [:refresh_token, :expires_in, :expires_at, :expires].include? k.to_sym}
+          )
         else
           token
         end
