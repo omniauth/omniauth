58d5e143e740d07ce3f4b6390fe4cc3268644082
Michael Bleigh <michael@intridea.com>
2011-09-22 11:44:13 -0500

    Switch twitter back to /authenticate, Closes #466, Closes #478

--- oa-oauth/lib/omniauth/strategies/oauth/twitter.rb
+++ oa-oauth/lib/omniauth/strategies/oauth/twitter.rb
@@ -17,7 +17,7 @@ module OmniAuth
           :site => 'https://api.twitter.com',
         }
         options[:authorize_params] = {:force_login => 'true'} if options.delete(:force_login) == true
-        client_options[:authorize_path] = '/oauth/authorize' unless options[:sign_in] == false
+        client_options[:authorize_path] = '/oauth/authenticate' unless options[:sign_in] == false
         super(app, options[:name] || :twitter, consumer_key, consumer_secret, client_options, options, &block)
       end
 
