0743594cf8cdd15d1a523ddfa457fd82a6a4360b
David LaMacchia <dml@copious.com>
2011-10-28 12:50:42 -0700

    update version to 0.3.2.1.copious

--- oa-oauth/lib/omniauth/version.rb
+++ oa-oauth/lib/omniauth/version.rb
@@ -10,7 +10,7 @@ module OmniAuth
       PATCH = 2
     end
     unless defined?(::OmniAuth::Version::PRE)
-      PRE   = "3.copious"
+      PRE   = "1.copious"
     end
     unless defined?(::OmniAuth::Version::STRING)
       STRING = [MAJOR, MINOR, PATCH, PRE].compact.join('.')
