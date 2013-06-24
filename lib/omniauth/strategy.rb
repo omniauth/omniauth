88a110d5e9b6759fcde5b01394c007879816019d
Josh Jordan <josh.jordan@gmail.com>
2013-06-24 14:12:55 -0400

    Do not rescue around mock_call!

--- lib/omniauth/strategy.rb
+++ lib/omniauth/strategy.rb
@@ -170,14 +170,14 @@ module OmniAuth
     #
     # @param env [Hash] The Rack environment.
     def call!(env)
+      raise OmniAuth::NoSessionError.new("You must provide a session to use OmniAuth.") unless env['rack.session']
+
+      @env = env
+      @env['omniauth.strategy'] = self if on_auth_path?
+
+      return mock_call!(env) if OmniAuth.config.test_mode
+
       begin
-        raise OmniAuth::NoSessionError.new("You must provide a session to use OmniAuth.") unless env['rack.session']
-
-        @env = env
-        @env['omniauth.strategy'] = self if on_auth_path?
-
-        return mock_call!(env) if OmniAuth.config.test_mode
-
         return options_call if on_auth_path? && options_request?
         return request_call if on_request_path? && OmniAuth.config.allowed_request_methods.include?(request.request_method.downcase.to_sym)
         return callback_call if on_callback_path?
