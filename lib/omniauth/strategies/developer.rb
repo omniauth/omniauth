e0c2cd7c75ba741bcbb7318e104ec868454d7cc9
Thomas Walpole <twalpole@gmail.com>
2015-08-20 17:01:46 -0700

    Reduce minimum coverage due to new rack2? method being logically shortcut and fix rubocop warning so travis tests are green

--- lib/omniauth/strategies/developer.rb
+++ lib/omniauth/strategies/developer.rb
@@ -37,7 +37,7 @@ module OmniAuth
       def request_phase
         form = OmniAuth::Form.new(:title => 'User Info', :url => callback_path)
         options.fields.each do |field|
-          form.text_field field.to_s.capitalize.gsub('_', ' '), field.to_s
+          form.text_field field.to_s.capitalize.tr('_', ' '), field.to_s
         end
         form.button 'Sign In'
         form.to_response
