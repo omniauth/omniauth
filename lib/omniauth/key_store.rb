7f7a29445a4776edc3c830929e4fb55ac691009d
Tom Milewski <tmilewski@gmail.com>
2017-02-11 15:05:03 -0500

    Revert temporary Hashie fix

--- lib/omniauth/key_store.rb
+++ lib/omniauth/key_store.rb
@@ -8,15 +8,12 @@ module OmniAuth
       require 'hashie/version'
       return unless Gem::Version.new(Hashie::VERSION) >= Gem::Version.new('3.5.0')
 
-      # if respond_to?(:disable_warnings)
-      #   disable_warnings
-      # else
-      #   define_method(:log_built_in_message) { |*| }
-      #   private :log_built_in_message
-      # end
-
-      define_method(:log_built_in_message) { |*| }
-      private :log_built_in_message
+      if respond_to?(:disable_warnings)
+        disable_warnings
+      else
+        define_method(:log_built_in_message) { |*| }
+        private :log_built_in_message
+      end
     end
 
     # Disable on loading of the class
