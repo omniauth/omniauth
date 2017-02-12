55ce9ec0784c9c95c9fa05a8492f9d5fca3df4fc
Tom Milewski <tmilewski@gmail.com>
2017-02-12 03:51:20 -0500

    Reimplement Coveralls

--- spec/helper.rb
+++ spec/helper.rb
@@ -1,5 +1,11 @@
 if RUBY_VERSION >= '1.9'
   require 'simplecov'
+  require 'coveralls'
+
+  SimpleCov.formatters = [
+    SimpleCov::Formatter::HTMLFormatter,
+    Coveralls::SimpleCov::Formatter
+  ]
 
   SimpleCov.start do
     add_filter '/spec/'
