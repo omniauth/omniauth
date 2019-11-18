67100c349d4a4568ce794fec1cd0de6ee682cf28
Bobby McDonald <bobbymcwho@gmail.com>
2019-11-18 11:41:06 -0500

    Set maximum coverage drop to 0.01

--- spec/helper.rb
+++ spec/helper.rb
@@ -10,6 +10,7 @@ if RUBY_VERSION >= '1.9'
   SimpleCov.start do
     add_filter ['/spec/', '/vendor/', 'strategy_macros.rb']
     minimum_coverage(92.5)
+    maximum_coverage_drop(0.01)
   end
 end
 
