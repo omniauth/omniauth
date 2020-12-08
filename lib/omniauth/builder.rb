0f0bbb102a4f8951a2561774501b9b409c325da9
Bobby McDonald <bobbymcwho@gmail.com>
2020-12-08 14:40:14 -0500

    Use hash args

--- lib/omniauth/builder.rb
+++ lib/omniauth/builder.rb
@@ -38,7 +38,7 @@ module OmniAuth
       end
 
       args.last.is_a?(Hash) ? args.push(options.merge(args.pop)) : args.push(options)
-      allowed_paths = args.fetch(:allowed_paths, [])
+      allowed_paths = args.last.is_a?(Hash) ? args.last.fetch(:allowed_paths, []) : []
 
       if allowed_paths.any?
         allowed_paths.each do |path|
