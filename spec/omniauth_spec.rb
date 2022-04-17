148a971ac7c7be3d2ea670b44ba11d6b698b9357
面川泰明 <omokawa@senk-inc.co.jp>
2022-04-17 13:17:06 +0900

    done

--- spec/omniauth_spec.rb
+++ spec/omniauth_spec.rb
@@ -8,7 +8,6 @@ describe OmniAuth do
           include OmniAuth::Strategy
         end
       end.to change(OmniAuth.strategies, :size).by(1)
-      binding.b
       expect(OmniAuth.strategies.last).to eq(ExampleStrategy)
     end
   end
